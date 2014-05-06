62bc54244eece8dfb75ad7c93fc940af4d25af06
Marek Hulan <mhulan@redhat.com>
2014-05-06 14:30:59 +0200

    Add support for parsing definitions

--- test/manifest_file_factory.rb
+++ test/manifest_file_factory.rb
@@ -5,12 +5,12 @@ class ManifestFileFactory
 
   def self.build(content)
     key = content.hash
-    @manifests[key] or @manifests[key] = build_file(content)
+    @manifests[key] or (@manifests[key] = build_file(content))
     @manifests[key]
   end
 
   def self.build_file(content)
-    f = Tempfile.new(['testing_manifest', '.pp'])
+    f = Tempfile.new(["testing_manifest", '.pp'])
     f.write content
     f.close
     f
