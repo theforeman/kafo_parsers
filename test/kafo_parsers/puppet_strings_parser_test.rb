575885560438b0b7357c379c595895cdcc927346
archanaserver <ak31960466@gmail.com>
2023-07-07 14:22:03 +0530

    Add Ruby 3 compatibility

--- test/kafo_parsers/puppet_strings_parser_test.rb
+++ test/kafo_parsers/puppet_strings_parser_test.rb
@@ -18,98 +18,98 @@ module KafoParsers
 
           describe 'data structure' do
             let(:keys) { data.keys }
-            specify { keys.must_include :values }
-            specify { keys.must_include :validations }
-            specify { keys.must_include :docs }
-            specify { keys.must_include :parameters }
-            specify { keys.must_include :types }
-            specify { keys.must_include :groups }
-            specify { keys.must_include :conditions }
-            specify { keys.must_include :object_type }
+            specify { _(keys).must_include :values }
+            specify { _(keys).must_include :validations }
+            specify { _(keys).must_include :docs }
+            specify { _(keys).must_include :parameters }
+            specify { _(keys).must_include :types }
+            specify { _(keys).must_include :groups }
+            specify { _(keys).must_include :conditions }
+            specify { _(keys).must_include :object_type }
           end
 
           describe 'object_type' do
-            specify { data[:object_type].must_equal manifest[:type] }
+            specify { _(data[:object_type]).must_equal manifest[:type] }
           end
 
           let(:parameters) { data[:parameters] }
           describe 'parsed parameters' do
-            specify { parameters.must_include 'required' }
-            specify { parameters.must_include 'version' }
-            specify { parameters.must_include 'sub_version' }
-            specify { parameters.must_include 'undocumented' }
-            specify { parameters.must_include 'undef' }
-            specify { parameters.must_include 'debug' }
-            specify { parameters.wont_include 'documented' }
+            specify { _(parameters).must_include 'required' }
+            specify { _(parameters).must_include 'version' }
+            specify { _(parameters).must_include 'sub_version' }
+            specify { _(parameters).must_include 'undocumented' }
+            specify { _(parameters).must_include 'undef' }
+            specify { _(parameters).must_include 'debug' }
+            specify { _(parameters).wont_include 'documented' }
           end
 
           describe "parsed values" do
             let(:values) { data[:values] }
             it 'includes values for all parameters' do
-              parameters.each { |p| values.keys.must_include p }
+              parameters.each { |p| _(values.keys).must_include p }
             end
-            specify { values['required'].must_be_nil }
-            specify { values['version'].must_equal '1.0' }
-            specify { values['sub_version'].must_equal 'beta' }
-            specify { values['undef'].must_equal :undef }
-            specify { values['multivalue'].must_equal ['x', 'y'] }
-            specify { values['mapped'].must_equal({'apples' => 'oranges', 'unquoted' => :undef}) }
-            specify { values['debug'].must_equal 'true' }
-            specify { values['variable'].must_equal '$::testing::params::variable' }
+            specify { _(values['required']).must_be_nil }
+            specify { _(values['version']).must_equal '1.0' }
+            specify { _(values['sub_version']).must_equal 'beta' }
+            specify { _(values['undef']).must_equal :undef }
+            specify { _(values['multivalue']).must_equal ['x', 'y'] }
+            specify { _(values['mapped']).must_equal({'apples' => 'oranges', 'unquoted' => :undef}) }
+            specify { _(values['debug']).must_equal 'true' }
+            specify { _(values['variable']).must_equal '$::testing::params::variable' }
           end
 
           describe "parsed validations are not supported" do
             let(:validations) { data[:validations] }
-            specify { validations.must_be_empty }
+            specify { _(validations).must_be_empty }
           end
 
           describe "parsed documentation" do
             let(:docs) { data[:docs] }
-            specify { docs.keys.must_include 'documented' }
-            specify { docs.keys.must_include 'version' }
-            specify { docs.keys.must_include 'undef' }
-            specify { docs.keys.wont_include 'm_i_a' }
-            specify { docs.keys.wont_include 'undocumented' }
-            specify { docs['version'].must_equal ['some version number'] }
-            specify { docs['multiline'].must_equal ['param with multiline', 'documentation', 'consisting of 3 lines'] }
-            specify { docs['typed'].wont_include 'type:bool' }
+            specify { _(docs.keys).must_include 'documented' }
+            specify { _(docs.keys).must_include 'version' }
+            specify { _(docs.keys).must_include 'undef' }
+            specify { _(docs.keys).wont_include 'm_i_a' }
+            specify { _(docs.keys).wont_include 'undocumented' }
+            specify { _(docs['version']).must_equal ['some version number'] }
+            specify { _(docs['multiline']).must_equal ['param with multiline', 'documentation', 'consisting of 3 lines'] }
+            specify { _(docs['typed']).wont_include 'type:bool' }
           end
 
           describe "parsed groups" do
             let(:groups) { data[:groups] }
             specify do
               skip "No default grouping of YARD parameters" if manifest[:name] == "YARD class"
-              groups['version'].must_equal ['Parameters']
+              _(groups['version']).must_equal ['Parameters']
             end
-            specify { groups['debug'].must_equal ['Advanced parameters'] }
-            specify { groups['server'].must_equal ['Advanced parameters', 'MySQL'] }
-            specify { groups['file'].must_equal ['Advanced parameters', 'Sqlite'] }
-            specify { groups['log_level'].must_equal ['Extra parameters'] }
+            specify { _(groups['debug']).must_equal ['Advanced parameters'] }
+            specify { _(groups['server']).must_equal ['Advanced parameters', 'MySQL'] }
+            specify { _(groups['file']).must_equal ['Advanced parameters', 'Sqlite'] }
+            specify { _(groups['log_level']).must_equal ['Extra parameters'] }
           end
 
           describe "parsed types" do
             let(:types) { data[:types] }
-            specify { types['version'].must_equal 'Any' }
-            specify { types['typed'].must_equal 'boolean' }
-            specify { types['remote'].must_equal 'boolean' }
-            specify { types['multivalue'].must_match /^(array|Array\[String\])$/ }
-            specify { types['mapped'].must_match /^(hash|Hash\[String, Variant\[String, Integer\]\])$/ }
+            specify { _(types['version']).must_equal 'Any' }
+            specify { _(types['typed']).must_equal 'boolean' }
+            specify { _(types['remote']).must_equal 'boolean' }
+            specify { _(types['multivalue']).must_match /^(array|Array\[String\])$/ }
+            specify { _(types['mapped']).must_match /^(hash|Hash\[String, Variant\[String, Integer\]\])$/ }
           end
 
           describe "parsed conditions" do
             let(:conditions) { data[:conditions] }
-            specify { conditions['version'].must_be_nil }
-            specify { conditions['typed'].must_be_nil }
+            specify { _(conditions['version']).must_be_nil }
+            specify { _(conditions['typed']).must_be_nil }
             if manifest[:name] == "YARD class"
-              specify { conditions['remote'].must_be_nil }
-              specify { conditions['server'].must_equal '$remote' }
-              specify { conditions['username'].must_be_nil }
-              specify { conditions['password'].must_equal '$username != \'root\'' }
+              specify { _(conditions['remote']).must_be_nil }
+              specify { _(conditions['server']).must_equal '$remote' }
+              specify { _(conditions['username']).must_be_nil }
+              specify { _(conditions['password']).must_equal '$username != \'root\'' }
             else
-              specify { conditions['remote'].must_equal '$db_type == \'mysql\'' }
-              specify { conditions['server'].must_equal '$db_type == \'mysql\' && $remote' }
-              specify { conditions['username'].must_equal '$db_type == \'mysql\'' }
-              specify { conditions['password'].must_equal '$db_type == \'mysql\' && $username != \'root\'' }
+              specify { _(conditions['remote']).must_equal '$db_type == \'mysql\'' }
+              specify { _(conditions['server']).must_equal '$db_type == \'mysql\' && $remote' }
+              specify { _(conditions['username']).must_equal '$db_type == \'mysql\'' }
+              specify { _(conditions['password']).must_equal '$db_type == \'mysql\' && $username != \'root\'' }
             end
           end
         end
@@ -119,14 +119,14 @@ module KafoParsers
         let(:manifest) { "# e✗amp✓e\n" + BASIC_MANIFEST.sub('class testing(', 'class testingutf8(') }
         let(:data) { PuppetStringsModuleParser.parse(ManifestFileFactory.build(manifest).path) }
         let(:parameters) { data[:parameters] }
-        specify { parameters.must_include 'version' }
+        specify { _(parameters).must_include 'version' }
       end
 
       describe 'with no class parameters' do
         let(:manifest) { "class testing { }" }
         let(:data) { PuppetStringsModuleParser.parse(ManifestFileFactory.build(manifest).path) }
         let(:parameters) { data[:parameters] }
-        specify { parameters.must_equal [] }
+        specify { _(parameters).must_equal [] }
       end
 
       describe 'with relative path' do
@@ -136,7 +136,7 @@ module KafoParsers
           PuppetStringsModuleParser.parse(path.to_s)
         end
         let(:parameters) { data[:parameters] }
-        specify { parameters.must_include 'version' }
+        specify { _(parameters).must_include 'version' }
       end
     end
 
