d49e11c836ce0252182b10c68d9d723a0867883f
Archana Kumari <32739028+archanaserver@users.noreply.github.com>
2023-08-01 14:19:15 +0530

    Fix minitest deprecation warnings

--- test/kafo_parsers/parsers_test.rb
+++ test/kafo_parsers/parsers_test.rb
@@ -4,29 +4,29 @@ require 'kafo_parsers/parsers'
 module KafoParsers
   describe Parsers do
     describe ".all" do
-      specify { Parsers.all.must_be_kind_of Array }
+      specify { _(Parsers.all).must_be_kind_of Array }
     end
 
     describe ".find_available" do
       let(:parser1) { PuppetStringsModuleParser }
 
-      specify { Parsers.stub(:all, []) { Parsers.find_available.must_be_nil } }
+      specify { Parsers.stub(:all, []) { _(Parsers.find_available).must_be_nil } }
 
       specify do
         parser1.stub(:available?, true) do
-          Parsers.stub(:all, [parser1]) { Parsers.find_available.must_equal parser1 }
+          Parsers.stub(:all, [parser1]) { _(Parsers.find_available).must_equal parser1 }
         end
       end
 
       specify do
         parser1.stub(:available?, false) do
-          Parsers.stub(:all, [parser1]) { Parsers.find_available.must_be_nil }
+          Parsers.stub(:all, [parser1]) { _(Parsers.find_available).must_be_nil }
         end
       end
 
       specify do
         parser1.stub(:available?, Proc.new { raise KafoParsers::ParserNotAvailable.new('N/A') } ) do
-          Parsers.stub(:all, [parser1]) { Parsers.find_available.must_be_nil }
+          Parsers.stub(:all, [parser1]) { _(Parsers.find_available).must_be_nil }
         end
       end
     end
